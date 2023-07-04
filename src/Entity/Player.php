bff050a0af89b26e4331508aa5608958877edf31
Jachean Mihai <j234mediplant@gmail.com>
2023-07-05 00:10:23 +0300

    Nickname and Trophies in Team

---
+++ src/Entity/Player.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlayerRepository::class)]
+class Player
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $Shirtnumber = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getShirtnumber(): ?int
+    {
+        return $this->Shirtnumber;
+    }
+
+    public function setShirtnumber(int $Shirtnumber): static
+    {
+        $this->Shirtnumber = $Shirtnumber;
+
+        return $this;
+    }
+}
